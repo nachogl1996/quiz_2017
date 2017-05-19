0318f3022963da6c098ba04963371868cefee1ef
nachogl1996 <ignacio.guerra.lorenzana@alumnos.upm.es>
2017-05-19 18:04:50 +0200

    Tema 11

--- models/index.js
+++ models/index.js
@@ -4,8 +4,18 @@ var path = require('path');
 // Cargar ORM
 var Sequelize = require('sequelize');
 
-// Usando BBDD SQLite:
-var sequelize = new Sequelize("sqlite:///", {storage: "quiz.sqlite"});
+var url, storage;
+
+if(!procces.env.DATABASE_URL){//SI ESTAMOS EN LOCAL SE UTILIZA SQLITE
+    url="sqlite://";
+    storage = "quiz.sqlite";
+} else {//SI ESTAMOS EN EL ENTORNO DE HEROKU SE USA EL URL DE LA BBDD POSTGRES
+    url = procces.env.DATABASE_URL;
+    storage = procces.env.DATABASE_STORAGE || "";
+}
+var sequelize = new  Sequelize(url, {storage: storage});
+
+
 
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
