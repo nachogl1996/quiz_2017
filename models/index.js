f20f1548ee0d608dd637f6ea811bebafde67a6a6
nachogl1996 <ignacio.guerra.lorenzana@alumnos.upm.es>
2017-05-19 18:15:06 +0200

    Tema 11

--- models/index.js
+++ models/index.js
@@ -6,12 +6,12 @@ var Sequelize = require('sequelize');
 
 var url, storage;
 
-if(!procces.env.DATABASE_URL){//SI ESTAMOS EN LOCAL SE UTILIZA SQLITE
+if(!process.env.DATABASE_URL){//SI ESTAMOS EN LOCAL SE UTILIZA SQLITE
     url="sqlite://";
     storage = "quiz.sqlite";
 } else {//SI ESTAMOS EN EL ENTORNO DE HEROKU SE USA EL URL DE LA BBDD POSTGRES
-    url = procces.env.DATABASE_URL;
-    storage = procces.env.DATABASE_STORAGE || "";
+    url = process.env.DATABASE_URL;
+    storage = process.env.DATABASE_STORAGE || "";
 }
 var sequelize = new  Sequelize(url, {storage: storage});
 
